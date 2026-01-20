342960d6371c19448c49de356cae0305e6e50734
WhitesLove <kubai.rita5@gmail.com>
2026-01-20 17:06:05 +0300

    Added Dockerfile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     experimental: {appManifest: false},
     runtimeConfig: {
         public: {
-            apiBase: 'http://localhost:8002', // твой FastAPI
+            apiBase: 'http://localhost:8000',
         }
     }
 });
