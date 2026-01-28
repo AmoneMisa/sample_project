b5c7789526b00ffd31af6b6d0e229332db49d1a6
WhitesLove <kubai.rita5@gmail.com>
2026-01-28 11:57:27 +0300

    Logs

--- server/plugins/error-handler.ts
+++ server/plugins/error-handler.ts
@@ -1,5 +1,3 @@
-import { defineNitroPlugin } from 'nitropack/runtime';
-
 export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('error', (err, { event }) => {
         console.error('[NITRO_ERROR]', err);
