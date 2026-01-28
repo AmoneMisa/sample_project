3dd794275c9806617465cd2d899485f81d3d7d6d
WhitesLove <kubai.rita5@gmail.com>
2026-01-28 13:02:11 +0300

    Logs

--- app/plugins/i18n-api.ts
+++ app/plugins/i18n-api.ts
@@ -1,4 +1,5 @@
 import {loadInitialDataSSR} from "~/composables/useInitialLoad";
+import { toRaw, unref } from 'vue';
 
 export default defineNuxtPlugin(async (nuxtApp) => {
     const i18n = nuxtApp.$i18n;
@@ -12,7 +13,7 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 
         i18n.setLocaleMessage(lang, translations);
 
-        nuxtApp.payload.data.headerMenu = menu;
-        nuxtApp.payload.data.testimonials = testi;
+        nuxtApp.payload.data.headerMenu = toRaw(unref(menu));
+        nuxtApp.payload.data.testimonials = toRaw(unref(testi));
     }
 });
