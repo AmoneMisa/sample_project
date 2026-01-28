d7f6e740cccd92ee02865e813871817532d8ba32
WhitesLove <kubai.rita5@gmail.com>
2026-01-28 11:28:14 +0300

    Logs

--- app/plugins/vue-error-handler.ts
+++ app/plugins/vue-error-handler.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.hook('vue:error', (err) => {
         console.error('Uncaught error', err);
     });
-    nuxtApp.hook('app:error', (err) => {
-        console.error('[APP ERROR]', err);
-    })
+    nuxtApp.hook('app:error', (...args) => {
+        console.error('[APP ERROR]', ...args)
+    });
 });
