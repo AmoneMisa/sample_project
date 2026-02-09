54664dc5e9b86aa4191368043436b2cec8f8abf1
WhitesLove <kubai.rita5@gmail.com>
2026-02-10 00:08:38 +0300

    Added grammar check; Added conditions for safe fetch contacts

--- app/layout/UnderFooter.vue
+++ app/layout/UnderFooter.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import {safeFetch} from "~/utils/safeFetch";
 import ContactItem from "~/components/footer/ContactItem.vue";
+import {computed} from "vue";
 
 const config = useRuntimeConfig();
-const {data: contacts} = await safeFetch<[]>(
+const {data: contactsRow} = await safeFetch<[]>(
     `${config.public.apiBase}/contacts`
 );
+
+const contacts = computed(() => contactsRow?.contacts ?? []);
 </script>
 
 <template>
